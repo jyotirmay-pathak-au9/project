e71a4ed5547fe5a8b0078bdb5b5f0651e06781c7
jyotirmay <jyotirmay362@gmail.com>
2020-11-08 13:27:13 +0530

    UI Login form added

---
+++ fooddelivery/script.js
@@ -0,0 +1,28 @@
+var show = document.getElementById("nav-links")
+        function showMenu(){
+            show.style.right = "0";
+        }
+        function closeMenu(){
+            show.style.right = "-200px";
+        }
+        function logInForm()
+        {
+            window.location.href="loginForm.htm"
+        }
+
+        function fb()
+        {
+            window.location.href="https://www.facebook.com/"
+        }
+        function insta()
+        {
+            window.location.href="https://www.instagram.com/"
+        }
+        function twitter()
+        {
+            window.location.href="https://twitter.com/"
+        }
+        function pinterest()
+        {
+            window.location.href="https://in.pinterest.com/"
+        }
